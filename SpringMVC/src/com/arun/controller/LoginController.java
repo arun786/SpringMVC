a15b0a22526d4b49742ef78e0bd176f7eb2260f1
arun789 <adwiti1975@gmail.com>
2017-01-23 00:34:46 -0700

    step 2 for mvc

--- SpringMVC/src/com/arun/controller/LoginController.java
+++ SpringMVC/src/com/arun/controller/LoginController.java
@@ -1,16 +1,31 @@
 package com.arun.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
 
-	@RequestMapping(value = "/login")
-	@ResponseBody /*This will serve as payload for the page*/
+	@RequestMapping(value = "/logins")
+	@ResponseBody /* This will serve as payload for the page */
 	public String getLoginPage() {
+		return "Hello World";
+	}
+
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String loginPage() {
 		return "login";
+	}
+
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public String loginPages(ModelMap map, @RequestParam String name, @RequestParam String password) {
+		map.put("name", name);
+		map.put("password", password);
+		return "welcome";
 	}
 
 }
